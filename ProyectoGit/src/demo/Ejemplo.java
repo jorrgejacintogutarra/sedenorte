d7dd0a5d0fc073acbcbcafaa1620b1893dfb9bed
SUITE <SUITE@SUITEE102-SERV.cibertec.edu.pe>
2018-10-10 20:39:23 -0500

    Nueasasasvo cambio

---
+++ ProyectoGit/src/demo/Ejemplo.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Ejemplo {
+
+	public static void main(String[] args) {
+		System.out.println("Git Gub");
+	}
+	
+	public void metodo1() {
+		
+	}
+	public void metodo2() {
+		
+	}
+}
